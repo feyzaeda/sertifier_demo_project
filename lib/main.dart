2f4de5da2c939f8268b0e101870b22606bb1e3d1
feyzaeda <tfeyzaeda@gmail.com>
2020-07-04 03:43:28 +0300

    added empty page2 and page3

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class _MainPageState extends State<MainPage> {
       ),
       body: feedPage(),
 
-      bottomNavigationBar: footerComponent(),
+      bottomNavigationBar: bottomNavigationBarComponent(),
 
     );
   }
---
+++ lib/screens/page3.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:sertifier_demo_project/components/appBarComponent.dart';
+import 'package:sertifier_demo_project/components/bottomNavigationBarComponent.dart';
+
+class page3 extends StatefulWidget{
+  @override
+  page3State createState() => page3State();
+
+}
+
+class page3State extends State<page3> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: <Widget>[
+            Expanded(
+              child: appBarComponent(),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: bottomNavigationBarComponent(),
+    );
+  }
+}
