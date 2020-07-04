d25859dbd42e985717ed3331edcf8789113b1dd4
feyzaeda <tfeyzaeda@gmail.com>
2020-07-04 15:17:56 +0300

    added appData.json

--- lib/screens/feedDetailPage.dart
+++ lib/screens/feedDetailPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sertifier_demo_project/components/appBarComponent.dart';
+import 'dart:convert';
 import 'package:sertifier_demo_project/components/footerComponent.dart';
 
 class feedDetailPage extends StatefulWidget{
@@ -8,9 +10,35 @@ class feedDetailPage extends StatefulWidget{
 }
 
 class feedDetailPageState extends State<feedDetailPage> {
+  List data;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: <Widget>[
+            appBarComponent()
+          ],
+        ),
+      ),
+      body: Center(
+        child: FutureBuilder(
+          future: DefaultAssetBundle.of(context).loadString('assets/data/appData.json'),
+          builder: (contex, snapshot){
+            data = jsonDecode(snapshot.data.toString());
+            return ListView.builder(itemBuilder: (BuildContext context, int index){
+              //if(snapshot = 'upperFeed'){}
+              return Card(
+                child: Column(
+                  children: <Widget>[
+
+                  ],
+                ),
+              );
+            });
+          },
+        ),
+      ),
       bottomNavigationBar: footerComponent(),
     );
   }
